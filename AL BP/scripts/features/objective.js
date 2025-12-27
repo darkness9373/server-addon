988b8b586db118eb1d9cf4497647ceb9615231e1
Darkness <darkness.9373@gmail.com>
2025-12-27 18:58:40 +0700

    no ignore

--- AL BP/scripts/features/objective.js
+++ AL BP/scripts/features/objective.js
@@ -1,9 +1,3 @@
 import { world } from '@minecraft/server';
 import Obj from '../extension/Objective.js';
 
-world.afterEvents.worldLoad.subscribe(() => {
-  Obj.add('money')
-  Obj.add('moneyRaw')
-  Obj.add('killMonster')
-  Obj.add('killMob')
-})
