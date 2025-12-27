51a46b87feb9aee5d820ff3bbc30a72668ee1f18
Darkness <darkness.9373@gmail.com>
2025-12-27 19:40:41 +0700

    sc

--- AL BP/scripts/extension/Score.js
+++ AL BP/scripts/extension/Score.js
@@ -1,4 +1,4 @@
-import { world, system, Player } from '@minecraft/server'
+import { world, Entity, Player } from '@minecraft/server'
 
 class Score {
     /**
@@ -10,18 +10,30 @@ class Score {
     add(player, objective, amount) {
         this.set(player, objective, this.get(player, objective) + amount)
     }
+    /**
+     * 
+     * @param {Player} player 
+     * @param {string} objective 
+     * @param {number} amount 
+     */
     set(player, objective, amount) {
         let obj = world.scoreboard.getObjective(objective)
         if (!obj) {
             world.scoreboard.addObjective(objective)
         }
-        obj.setScore(player, amount)
+        obj.setScore(player.scoreboardIdentity, amount)
     }
     remove(player, objective, amount) {
         this.set(player, objective, this.get(player, objective) - amount)
     }
+    /**
+     * 
+     * @param {Player} player 
+     * @param {string} objective 
+     * @returns 
+     */
     get(player, objective) {
-        return world.scoreboard.getObjective(objective).getScore(player) ?? 0
+        return world.scoreboard.getObjective(objective).getScore(player.scoreboardIdentity) ?? 0
     }
 }
 
