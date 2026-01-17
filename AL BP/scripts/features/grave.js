d47219315c13cb59c50d82b1846571165b5420f1
darkness9373 <ogustiantpharell@gmail.com>
2026-01-17 07:34:03 +0700

    grave up

--- AL BP/scripts/features/grave.js
+++ AL BP/scripts/features/grave.js
@@ -1,33 +1,65 @@
-import { world, BlockPermutation, Player } from "@minecraft/server";
-import { text } from "../config/text";
+import { world, BlockPermutation, ItemStack, Player } from "@minecraft/server";
+import { text } from '../config/text'
+
+const GRAVESTONE_ENTITY = 'drk:gravestone_item'
+const GRAVESTONE_BLOCK = 'drk:gravestone_block'
+
+world.beforeEvents.playerBreakBlock.subscribe(async data => {
+    const player = data.player;
+    const block = data.block
+    if (block.typeId !== GRAVESTONE_BLOCK) return;
+    await null;
+    
+    const entities = player.dimension.getEntities({
+        location: block.location,
+        type: GRAVESTONE_ENTITY,
+        maxDistance: 1
+    })
+    
+    if (entities.length === 0) return
+    
+    const grave = entities[0]
+    const ownerTag = `owner:${player.name}`
+    
+    if (!grave.hasTag(ownerTag)) {
+        data.cancel = true
+        player.sendMessage(
+            text('Ini bukan gravestone milikmu!').System.fail
+        )
+        return
+    }
+    
+    grave.kill()
+})
 
 world.afterEvents.entityDie.subscribe(data => {
     const player = data.deadEntity;
-    if (!(player instanceof Player)) return;
     const dim = player.dimension;
-    const loc = player.location;
-    const locx = Math.floor(loc.x)
-    const locy = Math.floor(loc.y)
-    const locz = Math.floor(loc.z)
-
-    const block = dim.getBlock(loc)
-    block.setPermutation(BlockPermutation.resolve('drk:gravestone'))
-
-    const grave = dim.spawnEntity(
-        'drk:gravestone_con',
-        {
-            x: locx,
-            y: locy,
-            z: locz
-        }
-    )
-
-    grave.nameTag = `Grave of ${player.name}`
-    grave.addTag(`owner:${player.name}`)
-
-    const items = dim.getEntities({
+    const pos = player.location;
+    
+    const block = dim.getBlock(pos)
+    
+    const grave = dim.spawnEntity(GRAVESTONE_ENTITY, {
+        x: pos.x + 0.5,
+        y: pos.y,
+        z: pos.z + 0.5
+    })
+    const graveInv = grave.getComponent('inventory').container;
+    const near = dim.getEntities({
+        location = pos,
         type: 'minecraft:item',
-        location: loc,
         maxDistance: 3
     })
+    player.sendMessage(text(`Kamu mati di §e${pos.x} ${pos.y} ${pos.z}`).System.fail)
+    block.setPermutation(BlockPermutation.resolve(GRAVESTONE_BLOCK))
+    grave.nameTag = `${player.name}`
+    
+    for (const item of near) {
+        if (graveInv.emptySlotsCount <= 0) {
+            break;
+        }
+        let itemStack = item.getComponent('item').itemStack;
+        graveInv.addItem(itemStack)
+        item.remove()
+    }
 })
